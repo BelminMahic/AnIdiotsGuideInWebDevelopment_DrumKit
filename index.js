d8aa70d79163f38ccaaef51d9e8c49da0db9b065
belmin.mahic <belmin.mahic@edu.fit.ba>
2020-03-09 13:24:11 +0100

    dodano i pritiskanje tipke na tastaturi da zasvira

--- index.js
+++ index.js
@@ -7,39 +7,48 @@ for(var i=0;i<document.querySelectorAll(".drum").length;i++)
 
         var btnInnerHtml=this.innerHTML;
 
-        switch (btnInnerHtml) {
-            case "w":
-                var audioFile=new Audio("sounds/crash.mp3");
-                audioFile.play();
-                break;
-            case "a":
-                var audioFile=new Audio("sounds/kick-bass.mp3");
-                audioFile.play();
-            break;
-            case "s":
-                var audioFile=new Audio("sounds/snare.mp3");
-                audioFile.play();
-                break;
-            case "d":
-                var audioFile=new Audio("sounds/tom-1.mp3");
-                audioFile.play();break;
-            case "j":
-                var audioFile=new Audio("sounds/tom-2.mp3");
-                audioFile.play();break;
-            case "k":
-                var audioFile=new Audio("sounds/tom-3.mp3");
-                audioFile.play();break;
-            case "l":
-                var audioFile=new Audio("sounds/tom-4.mp3");
-                audioFile.play();break;
-            default:
-                break;
-        }
-
-    });
+        keySound(btnInnerHtml);
+});
 }
 
 
+document.addEventListener("keypress",function(event){
+    keySound(event.key);
 
-/*var audioFile=new Audio("sounds/tom-1.mp3");
-audioFile.play();*/
+})
+
+
+function keySound(key)
+{
+    switch (key) {
+        case "w":
+            var audioFile=new Audio("sounds/crash.mp3");
+            audioFile.play();
+            break;
+        case "a":
+            var audioFile=new Audio("sounds/kick-bass.mp3");
+            audioFile.play();
+        break;
+        case "s":
+            var audioFile=new Audio("sounds/snare.mp3");
+            audioFile.play();
+            break;
+        case "d":
+            var audioFile=new Audio("sounds/tom-1.mp3");
+            audioFile.play();break;
+        case "j":
+            var audioFile=new Audio("sounds/tom-2.mp3");
+            audioFile.play();break;
+        case "k":
+            var audioFile=new Audio("sounds/tom-3.mp3");
+            audioFile.play();break;
+        case "l":
+            var audioFile=new Audio("sounds/tom-4.mp3");
+            audioFile.play();break;
+        default:
+            break;
+    }
+
+}
+
+
